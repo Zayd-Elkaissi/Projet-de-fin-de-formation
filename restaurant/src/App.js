4468071fdf63257879995ca60f788ef915c9d7b3
zaydelkassi <92026156+zaydelkassi@users.noreply.github.com>
2023-06-10 13:56:09 +0100

    add

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -84,9 +84,9 @@ function App() {
                 <a href="#book-a-table" className="btn-book animated fadeInUp scrollto">Book a Table</a>
               </div>
             </div>
-            {/* <!-- <div className="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
-          <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" className="glightbox play-btn"></a>
-        </div> --> */}
+             <div className="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
+          <a href="" className="glightbox play-btn"></a>
+        </div> 
 
           </div>
         </div>
